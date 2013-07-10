c4e71a092c03cc6f517caeb61910edb5073876af
Reza Bacchus <BacchusReza@gmail.com>
2013-07-10 17:29:08 +0000

    Attempt #3 on web.js

--- web.js
+++ web.js
@@ -2,12 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var fs = require('fs');
-
-var data = fs.readFile('index.html', "utf-8");
-
 app.get('/', function(request, response) {
-  response.send(data);
+/*  response.send('Hello World!'); */
+  response.send(fs.readFileSync('/index.html',"utf-8"));
 });
 
 var port = process.env.PORT || 5000;
